edac4ac242b263e5328b81a05b1260b0e3f4b797
blen_meku <mekuriablen@gmail.com>
2021-03-31 23:16:01 -0700

    modified

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget(max) {
+  //   debugger;
+  return Math.floor(Math.random() * max);
+  //   return 1 + 1;
+}
+
+console.log(generateTarget(9));
+
+function compareGuesses(humanScore, computerScore, currentRoundNumber) {
+  //if else
+  if (Math.abs(humanScore) == currentRoundNumber) {
+    return true;
+  } else if (Math.abs(computerScore) == currentRoundNumber) {
+    return false;
+  }
+}
+console.log(compareGuesses());
+// function updateScore(winner){
+//  if(winner === 'human' || winner === 'computer'){
+//   compareGuesses()++
+//  }
+// }
+
+function advanceRound() {
+  currentRoundNumber += 1;
+}
+console.log(advanceRound());
